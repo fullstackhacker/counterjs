4079aa412e8c9db2cfd4c83c15efc508baddcb9d
kapadiamush <kapadiamush@gmail.com>
2016-04-12 04:57:12 -0400

    initial skeleton

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(500).json({
+        message: err.message,
+        error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(500).json({
+    message: err.message,
+    error: err
+  });
+});
+
+
+module.exports = app;
